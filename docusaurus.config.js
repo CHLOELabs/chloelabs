0cbedf4af4433253e84088e84d012540a576dee5
Amanda Tan <atan@internet2.edu>
2026-07-25 17:32:20 -0700

    Migrate ChloeLabs to custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://chloelabs.github.io/',
+  url: 'https://chloelabs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/chloelabs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
